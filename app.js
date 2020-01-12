5aa7e5598203691f8b3a27ebe6cfd885e68c1b9d
Aishwarya Majjagi <45104004+amajjagi@users.noreply.github.com>
2020-01-12 18:57:32 +0530

    comments

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require('express');
 
 var app = express();
 const bodyparser = require('body-parser');
-const db = require('./mysql2/database');
+// const db = require('./mysql2/database');
+
+//to get json data from myjson.com
 var getJSON = require('get-json')
+
+// To allow access control origins - Start
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
+///End////
 app.use(express.json())
 
 app.use(express.static('./forms'))
